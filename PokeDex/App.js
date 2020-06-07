42ee489752718162730f396e7dfd63085c1d3aab
Adam Rzońca <adam.rzonca@gmail.com>
2020-06-07 11:23:58 +0200

    Lab1

---
+++ PokeDex/App.js
@@ -0,0 +1,69 @@
+import React, {useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Platform,
+  FlatList,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+
+import {fetchPokemonsList} from './apiService';
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetchPokemonsList();
+      setData(response.results);
+    })();
+  }, []);
+
+  const barStyle = Platform.OS === 'ios' ? 'default' : 'light-content';
+
+  return (
+    <>
+      <StatusBar barStyle={barStyle} backgroundColor="black" />
+      <SafeAreaView style={styles.appContainer}>
+        <FlatList
+          data={data}
+          keyExtractor={(item, index) => item.name + index}
+          renderItem={({item, index, separator}) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  Alert.alert(item.name, item.url);
+                }}
+                key={index}
+                style="itemContainer">
+                <Text style={styles.text}>{item.name}</Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  appContainer: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  container: {
+    backgroundColor: '#eee',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  text: {fontSize: 30, fontWeight: '100'},
+  itemContainer: {padding: '8'},
+});
+
+export default App;
