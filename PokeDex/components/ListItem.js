ea6859c5a21d00c3f3c264ceb26a8ccd7460b1c9
Adam Rzońca <adam.rzonca@gmail.com>
2020-06-29 23:03:46 +0200

    Pokemon image as base64

--- PokeDex/components/ListItem.js
+++ PokeDex/components/ListItem.js
@@ -8,19 +8,43 @@ import {
   Image,
   //   AbortController,
 } from 'react-native';
+
+import AsyncStorage from '@react-native-community/async-storage';
+import RNFetchBlob from 'react-native-fetch-blob';
+
 import {fetchPokemon} from '../apiService';
+import {useAsyncStorage} from '../hooks/useAsyncStorage';
 
 export const ListItem = ({navigation, item, index, isRefreshing}) => {
   const [pokemon, setPokemon] = useState(null);
+
   useEffect(() => {
     // const controller = new AbortController();
     // const signal = controller.signal;
 
     (async () => {
       //   const response = await fetchPokemon(item.url, signal);
-      const response = await fetchPokemon(item.url);
+      if (item) {
+        console.log(item.url);
 
-      setPokemon(response);
+        const response = await fetchPokemon(item.url);
+
+        // const base64Image = await ImgToBase64.getBase64String(
+        //   response.sprites.front_default,
+        // );
+
+        const result = await RNFetchBlob.fetch(
+          'GET',
+          response.sprites.front_default,
+        );
+
+        const base64Image = result.data;
+        console.log(base64Image);
+
+        response.base64Image = base64Image;
+
+        setPokemon(response);
+      }
     })();
     // return () => controller.abort();
   }, []);
@@ -33,7 +57,8 @@ export const ListItem = ({navigation, item, index, isRefreshing}) => {
       <>
         <Image
           style={styles.image}
-          source={{uri: pokemon.sprites.front_default}}
+          //source={{uri: pokemon.sprites.front_default}}
+          source={{uri: `data:image/png;base64,${pokemon.base64Image}`}}
         />
         <Text>{pokemon.id}</Text>
       </>
